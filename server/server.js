734cbffedd82a97a251c3fa4f04130777139b46e
Eray Aydın <eray@erayaydin.me>
2015-07-20 20:42:11 +0000

    Added public path and express basic route

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
+var path    = require('path');
 
 var config  = require('./config.json');
 
-app.use(express.static(__dirname + '/../client'));
+app.get('/', function(req, res) {
+	res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+});
+
+app.use(express.static(__dirname + '/../public'));
 
 io.on('connection', function(socket) {
 	console.log("New connection!");
